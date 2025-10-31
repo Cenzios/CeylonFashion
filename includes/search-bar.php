f4f1d8c119332ce6b4a3abdb570c33746001d4a9
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-31 15:21:10 +0530

    Add search bar component for enhanced product search functionality

---
+++ includes/search-bar.php
@@ -0,0 +1,10 @@
+<div class="container-fluid pt-3">
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-4">
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 search-input" type="search" placeholder="Search by colour or Name" aria-label="Search" style="height: 38px;" />
+        <button class="btn btn-search" type="submit" style="height: 38px;">Search</button>
+      </form>
+    </div>
+  </div>
+</div>
