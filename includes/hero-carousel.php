7d7a84c829d5ec09a47e686e2a9e001e0f35b527
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-31 15:21:24 +0530

    Add hero carousel component for enhanced visual display of images

---
+++ includes/hero-carousel.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-interval="600" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="Images/slider image-1.png" class="d-block w-100 banner-img" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="Images/slider image-2.png" class="d-block w-100 banner-img" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="Images/slider image-3.jpg" class="d-block w-100 banner-img" alt="...">
+      </div>
+    </div>
+  </div>
+</div>
