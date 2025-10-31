8a444cc87c54409e263f748dbd33d60e975856e9
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-31 15:21:37 +0530

    Add promo cards component for promoting reselling and custom outfit design

---
+++ includes/promo-cards.php
@@ -0,0 +1,40 @@
+<div class="container-fluid" style="padding: 0;">
+  <div class="row g-3 pt-3">
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <img src="Images/Banner image -01.png" class="card-img-top" alt="Background Image">
+        <div class="card-body">
+          <h5 class="card-title">
+            Resell Your Ceylon-Fashion Outfit - Earn Up to 60% Back!
+          </h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Only for verified City Fashion buyers</li>
+            <li class="list-group-item">List within 3 months of purchase — zero fees!</li>
+            <li class="list-group-item">Admin-approved listings for trust & quality</li>
+            <li class="list-group-item">Give luxury outfits a second life & earn cash</li>
+          </ul>
+          <div class="mt-3 text-center">
+            <a href="#" class="btn btn-primary" id="startResellingBtn">Start Reselling</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <img src="Images/Banner image -2.png" class="card-img-top" alt="Background Image">
+        <div class="card-body">
+          <h5 class="card-title">
+            Design Your Perfect outfit with Ceylon Fashion
+          </h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Design Yours in 3 steps</li>
+            <li class="list-group-item">Tailor Every Garment to Your style and size</li>
+            <li class="list-group-item">Start by selecting Your favourite design</li>
+            <li class="list-group-item">Make Your dream day reality with Unique a</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
