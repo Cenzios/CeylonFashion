37f5a520ce3ef3f370cd84d6df58a31cca867355
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-11-04 15:22:01 +0530

    Refactor search-bar.php and create search-results.php for enhanced search functionality; update form structure, improve user experience, and implement search results display.

--- includes/search-bar.php
+++ includes/search-bar.php
@@ -1,9 +1,19 @@
 <div class="container-fluid pt-3">
   <div class="row">
     <div class="col-12 col-md-6 col-lg-4">
-      <form class="d-flex" role="search">
-        <input class="form-control me-2 search-input" type="search" placeholder="Search by colour or Name" aria-label="Search" style="height: 38px;" />
-        <button class="btn btn-search" type="submit" style="height: 38px;">Search</button>
+      <form class="d-flex" role="search" method="GET" action="search-results.php">
+        <input 
+          class="form-control me-2 search-input" 
+          type="search" 
+          name="search"
+          placeholder="Search by Name or Category" 
+          aria-label="Search" 
+          style="height: 38px;" 
+          required
+        />
+        <button class="btn btn-search" type="submit" style="height: 38px;">
+          Search
+        </button>
       </form>
     </div>
   </div>
