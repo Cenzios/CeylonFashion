8b53d35e307548ff25dc47a2ecf5e0a712aa3258
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-11-12 17:51:01 +0530

    fix: correct image paths in hero carousel for consistency

--- includes/hero-carousel.php
+++ includes/hero-carousel.php
@@ -2,13 +2,13 @@
   <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-interval="600" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="images/slider image-1.png" class="d-block w-100 banner-img" alt="...">
+        <img src="/images/slider image-1.png" class="d-block w-100 banner-img" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="images/slider image-2.png" class="d-block w-100 banner-img" alt="...">
+        <img src="/images/slider image-2.png" class="d-block w-100 banner-img" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="images/slider image-3.jpg" class="d-block w-100 banner-img" alt="...">
+        <img src="/images/slider image-3.jpg" class="d-block w-100 banner-img" alt="...">
       </div>
       <div></div>
     </div>
